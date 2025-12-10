547e4a0e771cfb234a053635b4131d3f3ce39af5
niazye <niazye@outlook.com>
2025-12-10 22:52:52 +0800

    feat: 部分声明 NFA类的部分成员声明 state结构体的声明和定义 transfer_t类型

--- lab1/DFA.h
+++ lab1/DFA.h
@@ -8,6 +8,8 @@
 #include <cassert>
 #include <iostream>
 #include <memory>
+struct state;
+typedef std::pair<char, std::shared_ptr<state>> transfer_t;
 
 void trim_inplace(std::string& str);
 
@@ -32,10 +34,12 @@ class RE_tree
     char value;
     std::unique_ptr<RE_tree> left;
     std::unique_ptr<RE_tree> right;
+    friend class NFA;
 public:
     RE_tree(RE_operator oper, char val = 0, std::unique_ptr<RE_tree> l = nullptr, std::unique_ptr<RE_tree> r = nullptr);
     RE_tree(RE pattern_obj);
     ~RE_tree();
+    operator bool() const;
 };
 
 class RE
@@ -50,6 +54,28 @@ public:
     void add_pattern_line(std::string input_pattern);
     void merge_patterns();
     void print_pattern();
+
+};
+
+struct state
+{
+    bool is_final;
+    std::list<transfer_t> transfer;
+};
+
+class NFA
+{
+    std::shared_ptr<state> start_state;
+    std::shared_ptr<state> final_state;
+public:
+    NFA(const RE_tree& re_tree);
+    ~NFA();
+    NFA(const NFA& other);
+    NFA(const char terminal);
+    bool onion(const NFA& other);
+    bool concat(const NFA& other);
+    bool kleene_star();
+    bool plus();
 
 };
 
